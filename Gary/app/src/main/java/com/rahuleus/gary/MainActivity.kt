9bc1d704395286a6b2db8dbbd788d43ae6910d94
Rahul <rahuleus>
2024-04-19 00:41:14 +0530

    Database and kinda finish

--- Gary/app/src/main/java/com/rahuleus/gary/MainActivity.kt
+++ Gary/app/src/main/java/com/rahuleus/gary/MainActivity.kt
@@ -1,31 +1,18 @@
 package com.rahuleus.gary
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContracts
 import com.google.mlkit.vision.barcode.common.Barcode
-import java.io.IOException
-import org.jsoup.Jsoup
-/*import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContract
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import com.rahuleus.gary.ui.theme.GaryTheme
-import androidx.compose.ui.tooling.preview.Preview*/
+import com.google.firebase.firestore.FirebaseFirestore
 import com.rahuleus.gary.databinding.ActivityMainBinding
-
-
 
 
 class MainActivity : ComponentActivity() {
 
     private val cameraPermission = android.Manifest.permission.CAMERA
     private lateinit var binding: ActivityMainBinding
-    private var we="https://in.openfoodfacts.org/product/"
 
 
     private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
@@ -37,9 +24,7 @@ class MainActivity : ComponentActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener{
-            requestCameraAndStartScanner()
-        }
+        binding.button.setOnClickListener{ requestCameraAndStartScanner() }
 
     }
 
@@ -60,63 +45,35 @@ class MainActivity : ComponentActivity() {
                         webReader(barcode.rawValue.toString())
                         //binding.textViewQrContent.text
                     }
-                    else ->{
-                        "INVALID".also { binding.textViewQrAllergen.text = it }
-                    }
                 }
             }
         }
     }
 
-    private fun webReader(bsite: String) {
-        val url = buildString {
-            append(we)
-            append(bsite)
-        }
+    private fun webReader(code: String) {
 
-        try {
-            val document = Jsoup.connect(url).get()
-            val content = "$document"
-            val words =content.split(" ")
-            extractor(words)
-        } catch (e: IOException) {
-            println("database found no match")
-            e.printStackTrace()
-        }
-    }
+        val db = FirebaseFirestore.getInstance()
+        val collection = "Product"
+        val field = "EAN13"
+        val query = db.collection(collection).whereEqualTo(field, code)
+        query.get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                for (document in task.result!!.documents) {
+                    val data = document.data
+                    val allergens = data?.get("Allergen")?.toString() ?: " "
+                    val ingredients = data?.get("ingredient")?.toString() ?: " "
 
-
-    private fun extractor(sentence: List<String>){
-        val Checker2= "Allergens"
-        val Checker="ingredients"
-        val breaker="Food"
-        var foundtarget=false
-        var foundtarget2=false
-        val Allergens=mutableListOf<String>()
-        val Ingredients= mutableListOf<String>()
-        for ( words in sentence) {
-            if(words == breaker)
-                break
-            else if (words == Checker) {
-                foundtarget= true
-            }
-            else if(foundtarget==true){
-                if(words == (Checker2)) {
-                    foundtarget=false
-                    foundtarget2=true
+                    binding.textViewQrAllergen.text = allergens
+                    binding.textViewQrIngredient.text = ingredients
                 }
-                else
-                Ingredients.add(words)
-            }
-            else if(foundtarget2==true){
-                Allergens.add(words)
-
+            } else {
+                "Error".also { binding.textViewQrAllergen.text = it }
+                "Error".also { binding.textViewQrIngredient.text = it }
             }
         }
-        binding.textViewQrAllergen.text = Allergens.toString()
-        binding.textViewQrIngredient.text = Ingredients.toString()
-
     }
+
+
 
     private fun requestCameraPermission() {
         when{
